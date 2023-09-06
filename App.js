8837afa284d846dfd6115ac9d7eaf73779e3d4dd
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-09-05 17:14:37 -0700

    First commit and Login screen

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    background: {
+      flex: 1,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+    },
+    contentContainer: {
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      padding: 16,
+      borderRadius: 8,
+    },
+    banner: {
+      fontSize: 36,
+      fontWeight: 'bold',
+      marginBottom: 32,
+      color: '#333333',
+      textAlign: 'center',
+    },
+    bannerContainer: {
+      position: 'absolute',
+      top: 50,
+      right: 20,
+    },
+    bannerImage: {
+      width: 70,
+      height: 70,
+      borderRadius: 15,
+      resizeMode: 'contain',
+    },
+    inputContainer: {
+      marginBottom: 16,
+    },
+    inputLabel: {
+      fontSize: 16,
+      marginBottom: 8,
+      color: '#333333',
+    },
+    input: {
+      width: 300,
+      height: 40,
+      borderColor: '#333333',
+      borderWidth: 1,
+      paddingHorizontal: 10,
+      borderRadius: 8,
+    },
+    
+    button: {
+      backgroundColor: '#00E6CF',
+      paddingVertical: 12,
+      paddingHorizontal: 24,
+      borderRadius: 8,
+      marginTop: 16,
+    },
+    buttonText: {
+      color: '#ffffff',
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./images/LoginBackground.jpg')} style={styles.background}>
+      <View style={styles.container}>
+          <View style={styles.bannerContainer}>
+          <Image source={require('./images/logo.png')} style={styles.bannerImage} />
+          </View>
+        <View style={styles.contentContainer}>
+          <Text style={styles.banner}>Login</Text>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Username</Text>
+            <TextInput style={styles.input} placeholder="Enter your username" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Password</Text>
+            <TextInput style={styles.input} placeholder="Enter your password" secureTextEntry={true} />
+          </View>
+          <TouchableOpacity style={styles.button} onPress={() => console.log("Login pressed")}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
