3e20d3aca7406ef348be94108f8552738fd14e62
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-09-18 14:57:51 -0700

    dynamically add the Maintenance stuff and add the Maintenance Details

--- components/MaintenanceHome.js
+++ components/MaintenanceHome.js
@@ -16,19 +16,25 @@ import {
   Modal,
 } from "react-native-paper";
 import DropDown from "react-native-paper-dropdown";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 import { UNITLIST, MAINTENANCEITEMS } from "../constants";
 
 const MaintenanceHome = () => {
   const [visible, setVisible] = React.useState(false);
   const [visible2, setVisible2] = React.useState(false);
+  const [visible3, setVisible3] = React.useState(false);
   const [checkedEnter, setCheckedEnter] = React.useState(false);
   const [checkedFire, setCheckedFire] = React.useState(false);
   const [checkedPet, setCheckedPet] = React.useState(false);
+  const [details, setDetails] = React.useState({});
   const [text, setText] = React.useState("");
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
   const showModal2 = () => setVisible2(true);
   const hideModal2 = () => setVisible2(false);
+  const showModal3 = () => setVisible3(true);
+  const hideModal3 = () => setVisible3(false);
   const [showDropDown, setShowDropDown] = React.useState(false);
   const [showDropDown2, setShowDropDown2] = React.useState(false);
   const [unit, setUnit] = React.useState("");
@@ -59,7 +65,7 @@ const MaintenanceHome = () => {
   const handleSegmentedButtonChange = (value) => {
     if (value === "upload") {
     } else if (value === "submit") {
-      handleAddItem()
+      handleAddItem();
     }
   };
   const handleAddItem = () => {
@@ -78,7 +84,11 @@ const MaintenanceHome = () => {
     setCheckedEnter(false);
     setCheckedPet(false);
     setCheckedFire(false);
-    hideModal2(true)
+    hideModal2(true);
+  };
+  const showDetails = (item) => {
+    setVisible3(true);
+    setDetails(item);
   };
 
   return (
@@ -105,6 +115,9 @@ const MaintenanceHome = () => {
         <ScrollView style={styles.scrollSection}>
           {inputList.map((item, index) => (
             <List.Item
+              onPress={() => {
+                showDetails(item);
+              }}
               key={index}
               title={item.title}
               description={item.description}
@@ -136,6 +149,50 @@ const MaintenanceHome = () => {
           >
             Delete
           </Button>
+        </Modal>
+        <Modal
+          visible={visible3}
+          onDismiss={hideModal3}
+          contentContainerStyle={containerStyle}
+        >
+          <View style={styles.modalContent}>
+            <Text style={styles.title}>
+              {details.unit} - {details.title}
+            </Text>
+            
+            <View style={styles.detailsContainer}>
+              <View style={styles.detailsColumn}>
+                <Icon
+                  name={details.fire ? "check" : "times"}
+                  size={16}
+                  color={details.fire ? "green" : "red"}
+                  style={styles.iconCheck}
+                />
+                <Text style={styles.iconText}>Fire</Text>
+              </View>
+              <View style={styles.detailsColumn}>
+                <Icon
+                  name={details.enter ? "check" : "times"}
+                  size={16}
+                  color={details.enter ? "green" : "red"}
+                  style={styles.iconCheck}
+                />
+                <Text style={styles.iconText}>Enter</Text>
+              </View>
+              <View style={styles.detailsColumn}>
+                <Icon
+                  name={details.pet ? "check" : "times"}
+                  size={16}
+                  color={details.pet ? "green" : "red"}
+                  style={styles.iconCheck}
+                />
+                <Text style={styles.iconText}>Pet</Text>
+              </View>
+            </View>
+            <View style={styles.descriptionContainer}>
+              <Text style={styles.description}>{details.description}</Text>
+            </View>
+          </View>
         </Modal>
         <Modal
           visible={visible2}
@@ -217,6 +274,13 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
     paddingHorizontal: 16,
   },
+  descriptionContainer: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    padding: 10,
+  },
   titleContainer: {
     flex: 1,
   },
@@ -226,8 +290,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
   },
   buttonContainer: {
     marginLeft: 10,
@@ -235,10 +301,38 @@ const styles = StyleSheet.create({
   scrollSection: {
     marginBottom: 40,
   },
+
+  modalContent: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 8,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  detailsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  detailsColumn: {
+    flex: 1,
+    // flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center", // Center align the columns horizontally
+  },
+  icon: {
+    marginRight: 8,
+  },
+  unit: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
 });
 const containerStyle = {
   backgroundColor: "white",
-  padding: 20,
+  padding: 10,
   borderRadius: 10,
   width: "80%",
   marginLeft: "auto",
