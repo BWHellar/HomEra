11b24efc58a77cc188770bb6f94f029444e0f40f
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-09-18 13:43:51 -0700

    Schedule modal now creates in a local list and sorts. Also added an ampty array screen

--- components/ScheduleHome.js
+++ components/ScheduleHome.js
@@ -1,22 +1,79 @@
 import React, { useState } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, FlatList } from "react-native";
 import { Calendar } from "react-native-calendars";
 import { Button, TextInput, Text, Portal, Modal } from "react-native-paper";
 import DatePicker from "react-native-modern-datepicker";
+import moment from "moment";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const ScheduleHome = () => {
   const [selectedDate, setSelectedDate] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [userInputDescription, setUserInputDescription] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [filteredScheduleItems, setFilteredScheduleItems] = useState([]);
+  const [scheduleItems, setScheduleItems] = useState([]);
+
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
-  const [visible, setVisible] = React.useState(false);
 
   const handleDayPress = (day) => {
     setSelectedDate(day.dateString);
+    const filteredItems = scheduleItems.filter(
+      (item) => item.id === day.dateString
+    );
+
+    // Order filteredItems by time in ascending order
+    const sortedItems = filteredItems.sort((a, b) => {
+      const timeA = moment(a.time, "h:mm A");
+      const timeB = moment(b.time, "h:mm A");
+      return timeA.diff(timeB);
+    });
+
+    setFilteredScheduleItems(sortedItems);
   };
 
-  const markedDates = {
-    [selectedDate]: { selected: true, selectedColor: "#00e6cf" },
+  const handleSubmit = () => {
+    if (selectedDate && userInput) {
+      const formattedTime = moment(selectedTime, "HH:mm").format("h:mm A");
+      const newItem = {
+        id: selectedDate.replace(/\//g, "-"),
+        time: formattedTime,
+        title: userInput,
+        description: userInputDescription,
+      };
+      setScheduleItems([...scheduleItems, newItem]);
+      setUserInput("");
+      setUserInputDescription("");
+      setSelectedDate("");
+      setSelectedTime("");
+      hideModal();
+    }
   };
+  const markedDates = {
+    [selectedDate]: {
+      selected: true,
+      selectedColor: "#00e6cf",
+    },
+  };
+  const renderScheduleItem = ({ item }) => (
+    <View style={styles.scheduleItem}>
+      <View style={styles.scheduleItemDetails}>
+        <Text style={styles.scheduleItemTime}>{item.time}</Text>
+        <Text style={styles.scheduleItemTitle}>{item.title}</Text>
+      </View>
+      <View style={styles.scheduleItemDescriptionContainer}>
+        <Icon
+          name="caret-right"
+          size={20}
+          color="#999"
+          style={styles.descriptionIcon}
+        />
+        <Text style={styles.scheduleItemDescription}>{item.description}</Text>
+      </View>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -44,85 +101,23 @@ const ScheduleHome = () => {
           markedDates={markedDates}
         />
       </View>
-      <ScrollView style={styles.scheduleContainer}>
-        {selectedDate && (
-          <>
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                9:00 AM - 10:00 AM: Meeting
-              </Text>
-            </View>
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                9:00 AM - 10:00 AM: Meeting
-              </Text>
-            </View>
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                9:00 AM - 10:00 AM: Meeting
-              </Text>
-            </View>
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                9:00 AM - 10:00 AM: Meeting
-              </Text>
-            </View>
+      {
+        filteredScheduleItems.length === 0 ? (
+          
+      <View style={styles.noItemsContainer}>
+        <Text style={styles.noItemsText}>Please Select a Date</Text>
+      </View>
+        ):(
 
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                4:00 PM - 5:00 PM: Interview
-              </Text>
-            </View>
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                2:00 PM - 3:00 PM: Presentation
-              </Text>
-            </View>
-
-            <View style={styles.scheduleItem}>
-              <Text style={styles.scheduleItemText}>
-                4:00 PM - 5:00 PM: Interview
-              </Text>
-            </View>
-          </>
-        )}
-      </ScrollView>
+      <View style={styles.scheduleContainer}>
+        <FlatList
+          data={filteredScheduleItems}
+          renderItem={renderScheduleItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+        )
+      }
       <Portal>
         <Modal
           visible={visible}
@@ -133,29 +128,36 @@ const ScheduleHome = () => {
           <TextInput
             label="Title"
             mode="outlined"
+            value={userInput}
+            onChangeText={setUserInput}
           />
-          <DatePicker onSelectedChange={(date) => setSelectedDate(date)} />
+          <DatePicker
+            onDateChange={(date) => setSelectedDate(date)}
+            onTimeChange={(time) => setSelectedTime(time)}
+            value={selectedDate}
+          />
           <TextInput
             label="Description"
             multiline
             mode="outlined"
+            onChangeText={setUserInputDescription}
+            value={userInputDescription}
             numberOfLines={4}
             style={{ minHeight: 100 }}
           />
           <Button
             compact
-            textColor="red"
             mode="contained-tonal"
             style={{ marginTop: 30 }}
+            onPress={handleSubmit}
           >
-            Delete
+            Submit
           </Button>
         </Modal>
       </Portal>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -166,6 +168,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  scheduleItemDetails: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   header: {
     flexDirection: "row",
@@ -191,15 +197,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#F8F8F8",
     marginBottom: 40,
   },
-  scheduleText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 8,
+  scheduleItemTime: {
+    marginRight: 5,
+  },
+  scheduleItemTitle: {
+    marginRight: 10,
+  },
+  scheduleItemDescription: {
+    marginLeft: 20,
   },
   line: {
     height: 1,
     backgroundColor: "gray",
     marginVertical: 8,
+  },
+  noItemsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  noItemsText: {
+    fontSize: 16,
   },
   scheduleItem: {
     marginBottom: 8,
@@ -209,6 +228,11 @@ const styles = StyleSheet.create({
   },
   scheduleItemText: {
     fontSize: 16,
+  },
+  scheduleItemDescriptionContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 20,
   },
 });
 
