0fbbbe3740a68c3f0f47920a833e6f8e5f81a868
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-09-20 13:35:15 -0700

    This will add an accounting screen that can be expanded with real data later

--- components/AccountingHome.js
+++ components/AccountingHome.js
@@ -1,12 +1,147 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import React from "react";
+import {
+  View,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Text,
+  Dimensions,
+  StyleSheet,
+} from "react-native";
+import DropDown from "react-native-paper-dropdown";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+import { InvoiceList, MonthList } from "../constants";
 const AccountingHome = () => {
+  const [user, setUser] = React.useState(InvoiceList[0]);
+  const [showDropDown, setShowDropDown] = React.useState(false);
+
+  const data = Array.from({ length: 6 }, () =>
+    (Math.random() * 100).toFixed(2)
+  );
+  const currentMonth = MonthList.slice(
+    new Date().getMonth() - 5,
+    new Date().getMonth() + 1
+  ).map((month) => month);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Accounting Page!</Text>
-    </View>
+    <ImageBackground
+      source={require("../images/gradient.png")}
+      style={styles.background}
+    >
+      <View style={{ paddingHorizontal: 16 }}>
+        <DropDown
+          label={user.label}
+          mode={"outlined"}
+          visible={showDropDown}
+          showDropDown={() => setShowDropDown(true)}
+          onDismiss={() => setShowDropDown(false)}
+          value={user}
+          setValue={setUser}
+          list={InvoiceList}
+        />
+      </View>
+      <View style={[styles.container, { alignItems: "center" }]}>
+        <LineChart
+          data={{
+            labels: currentMonth,
+            datasets: [{ data: data }],
+          }}
+          width={Dimensions.get("window").width - 10} // from react-native
+          height={300}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            // backgroundColor: "#00e6cf",
+            backgroundGradientFrom: "rgba(0, 161, 145, 0.1)",
+            backgroundGradientTo: "rgba(0, 80, 72, 0.1)",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={[styles.headerText, styles.leftAlign]}>Date</Text>
+          <Text style={[styles.headerText, styles.rightAlign]}>
+            {user.label|| user}
+          </Text>
+        </View>
+        {data.map((item, index) => (
+          <>
+          <View style={styles.row} key={item.id}>
+            <Text style={[styles.cell, styles.leftAlign]}>
+              {currentMonth[index]}
+            </Text>
+            <Text style={[styles.cell, styles.rightAlign]}>${item}k</Text>
+          </View>
+          <View style={styles.line} />
+          </>
+        ))}
+      </View>
+    </ImageBackground>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 50,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+  },
+  leftAlign: {
+    textAlign: "left",
+  },
+  rightAlign: {
+    textAlign: "right",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 17,
+  },
+  cell: {
+    flex: 1,
+    fontSize: 14,
+  },
+});
 
 export default AccountingHome;
--- constants.js
+++ constants.js
@@ -1,96 +1,125 @@
 export const API_URL = "https://api.example.com";
 export const MAX_RESULTS = 10;
 export const UNITLIST = [
-    {
-      label: "Unit 1",
-      value: "1",
-    },
-    {
-      label: "Unit 2",
-      value: "2",
-    },
-    {
-      label: "Unit 3",
-      value: "3",
-    },
-  ];
+  {
+    label: "Unit 1",
+    value: "1",
+  },
+  {
+    label: "Unit 2",
+    value: "2",
+  },
+  {
+    label: "Unit 3",
+    value: "3",
+  },
+];
+export const InvoiceList = [
+  {
+    label: "Invoices",
+    value: "Invoices",
+  },
+  {
+    label: "Debit",
+    value: "Debit",
+  },
+  {
+    label: "Credit",
+    value: "Credit",
+  },
+];
+
+export const MonthList = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 export const MAINTENANCEITEMS = [
-    {
-      title: "Fridge",
-      description: "Item description",
-      leftIcon: "fridge",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Doors",
-      description: "Item description",
-      leftIcon: "door",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Washer",
-      description: "Item description",
-      leftIcon: "dishwasher",
-      rightIcon: "dots-horizontal",
-    },
+  {
+    title: "Fridge",
+    description: "Item description",
+    leftIcon: "fridge",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Doors",
+    description: "Item description",
+    leftIcon: "door",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Washer",
+    description: "Item description",
+    leftIcon: "dishwasher",
+    rightIcon: "dots-horizontal",
+  },
 
-    {
-      title: "Faucet",
-      description: "Item description",
-      leftIcon: "water-pump",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Stove",
-      description: "Item description",
-      leftIcon: "stove",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Paint",
-      description: "Item description",
-      leftIcon: "format-paint",
-      rightIcon: "dots-horizontal",
-    },
+  {
+    title: "Faucet",
+    description: "Item description",
+    leftIcon: "water-pump",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Stove",
+    description: "Item description",
+    leftIcon: "stove",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Paint",
+    description: "Item description",
+    leftIcon: "format-paint",
+    rightIcon: "dots-horizontal",
+  },
 
-    {
-      title: "Lightbulb",
-      description: "Item description",
-      leftIcon: "lightbulb",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Locks",
-      description: "Item description",
-      leftIcon: "lock",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "External",
-      description: "Item description",
-      leftIcon: "grass",
-      rightIcon: "dots-horizontal",
-    },
+  {
+    title: "Lightbulb",
+    description: "Item description",
+    leftIcon: "lightbulb",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Locks",
+    description: "Item description",
+    leftIcon: "lock",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "External",
+    description: "Item description",
+    leftIcon: "grass",
+    rightIcon: "dots-horizontal",
+  },
 
-    {
-      title: "Windows",
-      description: "Item description",
-      leftIcon: "window-open-variant",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Trash",
-      description: "Item description",
-      leftIcon: "delete",
-      rightIcon: "dots-horizontal",
-    },
-    {
-      title: "Miscellaneous",
-      description: "Item description",
-      leftIcon: "domain",
-      rightIcon: "dots-horizontal",
-    },
-  ];
+  {
+    title: "Windows",
+    description: "Item description",
+    leftIcon: "window-open-variant",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Trash",
+    description: "Item description",
+    leftIcon: "delete",
+    rightIcon: "dots-horizontal",
+  },
+  {
+    title: "Miscellaneous",
+    description: "Item description",
+    leftIcon: "domain",
+    rightIcon: "dots-horizontal",
+  },
+];
 export const DEFAULT_OPTIONS = {
   headers: {
     "Content-Type": "application/json",
