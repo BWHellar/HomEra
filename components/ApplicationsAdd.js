c816045883a4735b3a0cddcdc4735b267d52389e
Brendan Hellar <47283372+BWHellar@users.noreply.github.com>
2023-09-27 14:39:08 -0700

    This will completet he second step of the application process. This will need to be revisited as it is too long so it must be refactored into a few different componenets

--- components/ApplicationsAdd.js
+++ components/ApplicationsAdd.js
@@ -4,9 +4,15 @@ import { TextInput, Button, Modal } from "react-native-paper";
 import DatePicker from "react-native-modern-datepicker";
 import DropDown from "react-native-paper-dropdown";
 import { PERSONLIST } from "../constants";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const ApplicationsAdd = () => {
   const [showDropDown, setShowDropDown] = useState(false);
+  const [showDropDown2, setShowDropDown2] = useState(false);
+  const [showDropDown3, setShowDropDown3] = useState(false);
+  const [showDropDown4, setShowDropDown4] = useState(false);
+  const [showDropDown5, setShowDropDown5] = useState(false);
+  const [showDropDown6, setShowDropDown6] = useState(false);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState("");
   const [inputs, setInputs] = useState({
@@ -20,17 +26,33 @@ const ApplicationsAdd = () => {
     Children: "",
     Pets: "",
   });
+  const [inputs2, setInputs2] = useState({
+    MobilePhone: "",
+    GovernmentId: "",
+    IDNumber: "",
+    IssuingState: "",
+    DOB: "",
+    MaritalStatus: "",
+    MonthlyRent: "",
+    From: "",
+    To: "",
+    LandlordName: "",
+    LandordEmail: "",
+    LandlordPhone: "",
+    Occupation: "",
+    Company: "",
+    FromCompany: "",
+    ToCompany: "",
+    Income: "",
+    SupervisorName: "",
+    SupervisorEmail: "",
+  });
 
   const handleInputChange = (field, value) => {
     setInputs((prevInputs) => ({
       ...prevInputs,
       [field]: value,
     }));
-  };
-
-  const handleDateConfirm = (date) => {
-    setSelectedDate(date);
-    setShowDatePicker(false);
   };
 
   return (
@@ -75,25 +97,25 @@ const ApplicationsAdd = () => {
             label={"Floor Plan"}
             mode={"outlined"}
             style={styles.input}
-            visible={showDropDown}
-            showDropDown={() => setShowDropDown(true)}
-            onDismiss={() => setShowDropDown(false)}
+            visible={showDropDown2}
+            showDropDown={() => setShowDropDown2(true)}
+            onDismiss={() => setShowDropDown2(false)}
             value={inputs.FloorPlan}
             setValue={(value) => handleInputChange("FloorPlan", value)}
             list={PERSONLIST}
           />
-           <DropDown
+          <DropDown
             label={"Unit Number"}
             mode={"outlined"}
             style={styles.input}
-            visible={showDropDown}
-            showDropDown={() => setShowDropDown(true)}
-            onDismiss={() => setShowDropDown(false)}
+            visible={showDropDown3}
+            showDropDown={() => setShowDropDown3(true)}
+            onDismiss={() => setShowDropDown3(false)}
             value={inputs.UnitNumber}
             setValue={(value) => handleInputChange("UnitNumber", value)}
             list={PERSONLIST}
           />
-<TextInput
+          <TextInput
             style={styles.input}
             label="Adults"
             mode="outlined"
@@ -116,20 +138,216 @@ const ApplicationsAdd = () => {
           />
         </View>
         <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Section 2</Text>
+          <Text style={styles.sectionTitle}>Primary Applicant</Text>
           <TextInput
             style={styles.input}
-            label="Input 3"
+            label="First Name"
             mode="outlined"
-            value={inputs.input3}
-            onChangeText={(value) => handleInputChange("input3", value)}
+            value={inputs.FirstName}
+            onChangeText={(value) => handleInputChange("FirstName", value)}
           />
           <TextInput
             style={styles.input}
-            label="Input 4"
+            label="Last Name"
             mode="outlined"
-            value={inputs.input4}
-            onChangeText={(value) => handleInputChange("input4", value)}
+            value={inputs.LastName}
+            onChangeText={(value) => handleInputChange("LastName", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Mobile Phone"
+            mode="outlined"
+            value={inputs2.MobilePhone}
+            onChangeText={(value) => handleInputChange("MobilePhone", value)}
+          />
+          <DropDown
+            label={"Government Id"}
+            mode={"outlined"}
+            visible={showDropDown4}
+            style={styles.input}
+            showDropDown={() => setShowDropDown4(true)}
+            onDismiss={() => setShowDropDown4(false)}
+            value={inputs2.GovernmentId}
+            setValue={(value) => handleInputChange("GovernmentId", value)}
+            list={PERSONLIST}
+          />
+          <TextInput
+            style={styles.input}
+            label="ID Number"
+            mode="outlined"
+            value={inputs2.IDNumber}
+            onChangeText={(value) => handleInputChange("IDNumber", value)}
+          />
+          <DropDown
+            label={"Issuing State"}
+            mode={"outlined"}
+            visible={showDropDown5}
+            style={styles.input}
+            showDropDown={() => setShowDropDown5(true)}
+            onDismiss={() => setShowDropDown5(false)}
+            value={inputs2.IssuingState}
+            setValue={(value) => handleInputChange("IssuingState", value)}
+            list={PERSONLIST}
+          />
+          <TextInput
+            style={styles.input}
+            label="Date of Birth"
+            mode="outlined"
+            value={selectedDate}
+            onTouchStart={() => setShowDatePicker(true)}
+          />
+          <DropDown
+            label={"Marital Status"}
+            mode={"outlined"}
+            visible={showDropDown6}
+            style={styles.input}
+            showDropDown={() => setShowDropDown6(true)}
+            onDismiss={() => setShowDropDown6(false)}
+            value={inputs2.MaritalStatus}
+            setValue={(value) => handleInputChange("MaritalStatus", value)}
+            list={PERSONLIST}
+          />
+          <Text style={styles.sectionTitle}>Leasing History</Text>
+          <GooglePlacesAutocomplete
+            placeholder="Address"
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              console.log(data, details);
+            }}
+            query={{
+              key: "YOUR API KEY",
+              language: "en",
+            }}
+            styles={{
+              textInputContainer: {
+                backgroundColor: "white",
+                borderWidth: 1,
+                borderColor: "rgba(0, 0, 0, 0.3)",
+                borderRadius: 5,
+                paddingHorizontal: 10,
+              },
+              textInput: {
+                height: 40,
+                color: "black",
+              },
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            label="Monthly Rent"
+            mode="outlined"
+            value={inputs2.MonthlyRent}
+            onChangeText={(value) => handleInputChange("MonthlyRent", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="From"
+            mode="outlined"
+            value={selectedDate}
+            onTouchStart={() => setShowDatePicker(true)}
+          />
+          <TextInput
+            style={styles.input}
+            label="To"
+            mode="outlined"
+            value={selectedDate}
+            onTouchStart={() => setShowDatePicker(true)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Landlord Name"
+            mode="outlined"
+            value={inputs2.LandlordName}
+            onChangeText={(value) => handleInputChange("LandlordName", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Landlord Email"
+            mode="outlined"
+            value={inputs2.LandlordEmail}
+            onChangeText={(value) => handleInputChange("LandlordEmail", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="LandlordPhone"
+            mode="outlined"
+            value={inputs2.LandlordPhone}
+            onChangeText={(value) => handleInputChange("LandlordPhone", value)}
+          />
+          <Text style={styles.sectionTitle}>Employment History</Text>
+          <GooglePlacesAutocomplete
+            placeholder="Address"
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              console.log(data, details);
+            }}
+            query={{
+              key: "YOUR API KEY",
+              language: "en",
+            }}
+            styles={{
+              textInputContainer: {
+                backgroundColor: "white",
+                borderWidth: 1,
+                borderColor: "rgba(0, 0, 0, 0.3)",
+                borderRadius: 5,
+                paddingHorizontal: 10,
+              },
+              textInput: {
+                height: 40,
+                color: "black",
+              },
+            }}
+          />
+          <TextInput
+            style={styles.input}
+            label="Occupation"
+            mode="outlined"
+            value={inputs2.Occupation}
+            onChangeText={(value) => handleInputChange("Occupation", value)}
+          />
+          
+          <TextInput
+            style={styles.input}
+            label="Company"
+            mode="outlined"
+            value={inputs2.Company}
+            onChangeText={(value) => handleInputChange("Company", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Monthly Income"
+            mode="outlined"
+            value={inputs2.Income}
+            onChangeText={(value) => handleInputChange("Income", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Supervisor Name"
+            mode="outlined"
+            value={inputs.SupervisorName}
+            onChangeText={(value) => handleInputChange("SupervisorName", value)}
+          />
+           <TextInput
+            style={styles.input}
+            label="Supervisor Email"
+            mode="outlined"
+            value={inputs.SupervisorEmail}
+            onChangeText={(value) => handleInputChange("SupervisorEmail", value)}
+          />
+          <TextInput
+            style={styles.input}
+            label="From"
+            mode="outlined"
+            value={selectedDate}
+            onTouchStart={() => setShowDatePicker(true)}
+          />
+          <TextInput
+            style={styles.input}
+            label="To"
+            mode="outlined"
+            value={selectedDate}
+            onTouchStart={() => setShowDatePicker(true)}
           />
         </View>
         <Button mode="contained" onPress={() => console.log(inputs)}>
@@ -156,6 +374,7 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
+    paddingBottom: 60,
   },
   section: {
     marginBottom: 20,
